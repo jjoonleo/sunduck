dbf0b9b8f11023d9d2f69bb47462e1567ae53478
ejun <jjoonleo@gmail.com>
2024-09-11 17:37:44 +0900

    handle first number

--- clac.py
+++ clac.py
@@ -1,1 +1,10 @@
 inputString = input().strip()
+data = []
+
+if inputString[0].isdigit():
+  data.append(['+', 0])
+  for i in inputString:
+    if not i.isdigit():
+      break
+    data[-1][1] = data[-1][1] * 10 + int(i)
+
