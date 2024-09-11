0a5b769f48873c5d3632aaca23c6b9cb0a840cce
ejun <jjoonleo@gmail.com>
2024-09-11 17:38:06 +0900

    handle rest

--- clac.py
+++ clac.py
@@ -8,3 +8,17 @@ if inputString[0].isdigit():
       break
     data[-1][1] = data[-1][1] * 10 + int(i)
 
+i = 1
+
+while i < len(inputString):
+  if inputString[i] == '+' or inputString[i] == '-':
+    data.append([inputString[i], 0])
+    i += 1
+    while i < len(inputString):
+      if not inputString[i].isdigit():
+        break
+      data[-1][1] = data[-1][1] * 10 + int(inputString[i])
+      i += 1
+  else:
+    i += 1
+
