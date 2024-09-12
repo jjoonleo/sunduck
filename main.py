90c4f99dc94b5704827014d474e13782a3631a32
ejun <jjoonleo@gmail.com>
2024-09-12 18:49:49 +0900

    make vault class

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-import vault
+from vault import Vault
 
-my_vault = {"is_vault_opened": False, "content": None, "password": "1234"}
+vault = Vault()
 while True:
-    if my_vault['is_vault_opened']:
+    if vault.is_vault_opened:
         option = input(
             '금고가 열렸습니다. 다음중 하나의 숫자를 입력하세요.\n1. 물건 확인하기\n2. 물건 넣기\n3. 물건 삭제하기\n4. 비밀번호 변경\n5. 금고 잠그기고 종료하기\n')
         try:
             match int(option):
                 case 1:
-                    vault.get_content(my_vault)
+                    vault.get_content()
                 case 2:
-                    vault.put_content(my_vault)
+                    vault.put_content()
                 case 3:
-                    vault.delete_content(my_vault)
+                    vault.delete_content()
                 case 4:
-                    vault.change_password(my_vault)
+                    vault.change_password()
                 case 5:
-                    my_vault['is_vault_opened'] = False
+                    vault['is_vault_opened'] = False
                     print("금고를 잠그고 종료합니다.")
                     break
                 case _:
@@ -26,4 +26,4 @@ while True:
             continue
 
     else:
-        vault.open_vault(my_vault)
+        vault.open_vault()
--- vault.py
+++ vault.py
@@ -1,35 +1,37 @@
-def open_vault(vault):
-    password_input = input('금고를 열기 위해서 비밀번호를 입력하세요. 초기 비밀번호는 1234입니다.\n')
-    if vault['password'] == password_input:
-        vault['is_vault_opened'] = True
-    else:
-        print("비밀번호가 일치하지 않습니다.")
+class Vault:
+    def __init__(self, initial_password):
+        self.password = initial_password
+        self.is_vault_opened = False
+        self.content = None
 
-
-def get_content(vault):
-    if (vault['content'] == None):
-        print("금고가 비어있습니다. 물건을 넣어주세요\n")
-    else:
-        print(f"금고에 들어있는 물건은 아래와 같습니다. \n\n {vault['content']}")
-
-
-def put_content(vault):
-    if (vault['content'] == None):
-        vault['content'] = input("금고에 넣으실 물건을 알려주세요")
-    else:
-        print("금고가 비어있지 않습니다. 먼저 물건을 삭제해 주세요")
-
-
-def delete_content(vault):
-    vault['content'] = None
-    print("물건이 삭제되었습니다.")
-
-
-def change_password(vault):
-    while True:
-        tmp_password = input("변경할 새 비밀번호를 입력해주세요")
-        if tmp_password == input("비밀번호를 한 번 더 입력해 주세요"):
-            vault['password'] = tmp_password
-            break
+    def open_vault(self):
+        password_input = input('금고를 열기 위해서 비밀번호를 입력하세요. 초기 비밀번호는 1234입니다.\n')
+        if self.password == password_input:
+            self.is_vault_opened = True
         else:
-            print("비밀 번호가 일치하지 않습니다.")
+            print("비밀번호가 일치하지 않습니다.")
+
+    def get_content(self):
+        if (self.content == None):
+            print("금고가 비어있습니다. 물건을 넣어주세요\n")
+        else:
+            print(f"금고에 들어있는 물건은 아래와 같습니다. \n\n {self.content}")
+
+    def put_content(self):
+        if (self.content == None):
+            self.content = input("금고에 넣으실 물건을 알려주세요")
+        else:
+            print("금고가 비어있지 않습니다. 먼저 물건을 삭제해 주세요")
+
+    def delete_content(self):
+        self.content = None
+        print("물건이 삭제되었습니다.")
+
+    def change_password(self):
+        while True:
+            tmp_password = input("변경할 새 비밀번호를 입력해주세요")
+            if tmp_password == input("비밀번호를 한 번 더 입력해 주세요"):
+                self.password = tmp_password
+                break
+            else:
+                print("비밀 번호가 일치하지 않습니다.")
